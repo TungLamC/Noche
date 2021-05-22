d0e8d3767698a3f3982806b4e2671711f40b1ca3
TungLam <841213323@qq.com>
2021-05-23 01:37:06 +0800

    add lua54 to 3rdparty

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 
+extern "C"
+{
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
+}
+
 int main()
 {
-  std::cout << "Hello, World!" << std::endl;
+  lua_State* state = luaL_newstate();
+  luaL_openlibs(state);
+  luaL_dostring(state, "print('fuck')");
   return 0;
 }
