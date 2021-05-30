bcf950318715157d7e6f456074bd6ba0a71285f2
TungLam <841213323@qq.com>
2021-05-30 14:46:59 +0800

    stack_reference constructors

--- 3rdparty/moonfall/moonfall.hpp
+++ 3rdparty/moonfall/moonfall.hpp
@@ -8,116 +8,45 @@ extern "C"
 #include <lauxlib.h>
 }
 
-#include <type_traits>
-
 namespace moonfall {
-template<typename T>
-struct unqualified_pusher;
+
+inline bool xmovable(lua_State* left, lua_State* right) {
+  if (left == nullptr || right == nullptr || left == right)
+    return false;
+  else
+    return lua_topointer(left, LUA_REGISTRYINDEX) == lua_topointer(right, LUA_REGISTRYINDEX);
 }
-
-namespace moonfall::traits {
-template<typename T>
-using unqualified_t = std::remove_cv_t<std::remove_reference_t<T>>;
-}
-
-namespace moonfall::stack {
-template<typename Object, typename... Args>
-int push(lua_State* state, Object&& object, Args&& ... args) {
-  using pusher = unqualified_pusher<traits::unqualified_t<Object>>;
-  pusher::push(state, std::forward<Object>(object), std::forward<Args>(args)...);
-}
-
-template<typename T>
-struct unqualified_pusher {
-  static int push(lua_State* state) { return 0; }
-};
-
-template<>
-struct unqualified_pusher<const char*> {
-  static int push(lua_State* state, const char* string) {
-    lua_pushstring(state, string);
-    return 1;
-  }
-};
-}
-
-namespace moonfall::reference {
 
 struct raw_index {
-  raw_index(int index): index(index) {}
-  operator int() const { return index; }
-  int index;
-};
-
-struct upvalue_index {
-  upvalue_index(int index): index(lua_upvalueindex(index)) {}
+  raw_index(int index) noexcept: index(index) {}
   operator int() const { return index; }
   int index;
 };
 
 struct absolute_index {
-  absolute_index(lua_State* state, int index): index(lua_absindex(state, index)) {}
+  absolute_index(lua_State* state, int index) noexcept: index(lua_absindex(state, index)) {}
   operator int() const { return index; }
   int index;
 };
 
-inline bool xmovable(lua_State* left, lua_State* right) {
-  return left == nullptr || right == nullptr || left == right ?
-         false : lua_topointer(left, LUA_REGISTRYINDEX) == lua_topointer(right, LUA_REGISTRYINDEX);
-}
-
 class stack_reference {
 public:
   stack_reference() noexcept = default;
-  stack_reference(raw_index index) noexcept: index(index) {}
-  stack_reference(absolute_index index) noexcept: index(index) {}
+  stack_reference(lua_State* state, int index) noexcept: state(state), index(lua_absindex(state, index)) {}
   stack_reference(lua_State* state, raw_index index) noexcept: state(state), index(index) {}
   stack_reference(lua_State* state, absolute_index index) noexcept: state(state), index(index) {}
-  stack_reference(lua_State* state, int index) noexcept: state(state), index(absolute_index(state, index)) {}
 
-  stack_reference(lua_State* state, const stack_reference& other) noexcept: state(state) {
-    if (!other.is_valid()) {
-      index = 0;
-      return;
-    }
-    if (xmovable(state, other.lua_state())) {
-      lua_pushvalue(other.lua_state(), other.stack_index());
-      lua_xmove(other.lua_state(), state, 1);
-      index = absolute_index(state, -1);
-    } else {
-      index = other.stack_index();
-    }
-  }
-
-public:
-  int push(lua_State* state = nullptr) const noexcept {
-    state = state == nullptr ? this->state : state;
-    lua_pushvalue(state, index);
-    return 1;
-  }
-
-public:
-  int stack_index() const noexcept {
-    return index;
-  }
-
-  lua_State* lua_state() const noexcept {
-    return state;
-  }
-
-  bool is_valid() const noexcept {
-    return true;
-  }
-
-  const void* pointer() const noexcept {
-    return lua_topointer(state, index);
-  }
+  stack_reference(const stack_reference& other) noexcept = default;
+  stack_reference& operator=(const stack_reference& other) noexcept = default;
+  stack_reference(stack_reference&& other) noexcept = default;
+  stack_reference& operator=(stack_reference&& other) noexcept = default;
 
 private:
   lua_State* state = nullptr;
 
   int index = 0;
 };
+
 }
 
 #endif //NOCHE_MOONFALL_HPP
