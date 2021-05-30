1569eeb54f8da5d7073e0e8ef34e1b634aa95397
TungLam <841213323@qq.com>
2021-05-30 15:12:44 +0800

    stack reference

--- 3rdparty/moonfall/moonfall.hpp
+++ 3rdparty/moonfall/moonfall.hpp
@@ -9,6 +9,21 @@ extern "C"
 }
 
 namespace moonfall {
+enum class lua_type: int {
+  none          = LUA_TNONE,
+  lua_nil       = LUA_TNIL,
+  string        = LUA_TSTRING,
+  number        = LUA_TNUMBER,
+  thread        = LUA_TTHREAD,
+  boolean       = LUA_TBOOLEAN,
+  function      = LUA_TFUNCTION,
+  userdata      = LUA_TUSERDATA,
+  lightuserdata = LUA_TLIGHTUSERDATA,
+  table         = LUA_TTABLE,
+  poly          = -0xFFFF
+};
+}
+namespace moonfall {
 
 inline bool xmovable(lua_State* left, lua_State* right) {
   if (left == nullptr || right == nullptr || left == right)
@@ -42,11 +57,35 @@ public:
   stack_reference& operator=(stack_reference&& other) noexcept = default;
 
 public:
-  int push() const noexcept {
-    
+  int push(lua_State* state = nullptr) const noexcept {
+    state == nullptr ? this->state : state;
+    lua_pushvalue(state, index);
+    return 1;
+  }
+
+public:
+  lua_type lua_type() const noexcept {
+    return static_cast<moonfall::lua_type>(::lua_type(state, index));
   }
   
- 
+  lua_State* lua_state() const noexcept {
+    return state;
+  }
+  
+  int stack_index() const noexcept {
+    return index;
+  }
+  
+  bool is_valid() const noexcept {
+    return lua_type() != lua_type::lua_nil && lua_type() != lua_type::none;
+  }
+
+  const void* pointer() const noexcept {
+    return lua_topointer(state, index);
+  }
+
+public:
+
 
 private:
   lua_State* state = nullptr;
@@ -54,6 +93,13 @@ private:
   int index = 0;
 };
 
+inline bool operator==(const stack_reference& lhs, const stack_reference& rhs) {
+  return lua_compare(lhs.lua_state(), lhs.stack_index(), rhs.stack_index(), LUA_OPEQ) == 1;
+}
+
+inline bool operator!=(const stack_reference& lhs, const stack_reference& rhs) {
+  return !operator==(lhs, rhs);
+}
 }
 
 #endif //NOCHE_MOONFALL_HPP
