85191ff95099e0d3ccfd79073eb91ed5ad7a6eb6
donglin.shi <shidonglin@bytedance.com>
2021-06-24 12:23:54 +0800

    reflection reset

--- main.cpp
+++ main.cpp
@@ -10,29 +10,9 @@ using namespace Noche;
 
 namespace Noche
 {
-
-class O { };
-constexpr auto get_type_info(O*)
-{
-  return type_info("O", (O*)0, (base_list<dummy_base>*)0, sizeof(O));
-}
-class A {};
-constexpr auto get_type_info(A*)
-{
-  return type_info("A", (A*)0, (base_list<dummy_base>*)0, sizeof(A));
-}
-class B: O, A {};
-constexpr auto get_type_info(B*)
-{
-  return type_info("B", (B*)0, (base_list<O, A>*)0, sizeof(B));
-}
-
 }
 
 int main()
 {
-  type_of<B>.each_bases([](auto base, int level) {
-    Log::Info("{} {}", typeid(base).name(), level);
-  });
   return 0;
 }
--- std/reflection.h
+++ std/reflection.h
@@ -9,52 +9,16 @@ namespace Noche
 {
 using namespace std;
 
-struct dummy_base {};
+struct dummy {};
 
-template<typename T>
-constexpr void get_type_info(T*);
+template<typename Class>
+constexpr void get_type_info(Class*);
 
-template<typename T>
-constexpr auto type_of = get_type_info((T*)0);
-
-
-template<typename... T>
-struct base_list
-{
-  static constexpr auto bases = std::tuple<decltype(get_type_info((T*)0))...>{ type_of<T>... };
-
-  template<size_t I>
-  using type_at = tuple_element_t<I, std::tuple<T...>>;
-};
-
-constexpr auto get_type_info(dummy_base*)
-{
-  return type_info("dummy", (dummy_base*)0, (base_list<dummy_base>*)0, sizeof(dummy_base));
-}
-template<typename Class, typename... Bases>
+template<typename Meta, typename Class, typename... Bases>
 struct type_info
 {
+  using meta_t = Meta;
   using class_t = Class;
-  using bases_t = base_list<Bases...>;
-
-  string_view name;
-  size_t size;
-
-  constexpr type_info(string_view name, Class*, bases_t* bases, size_t size)
-    : name{name}, size{size}
-  {
-    // todo
-  }
-
-  template<typename F>
-  constexpr void each_bases(F&& visitor, int level = 0) const
-  {
-    if constexpr (is_same_v<dummy_base, typename bases_t::type_at<0>>)
-      return;
-    for_each_tuple(bases_t::bases, [=](auto base) {
-      visitor(base, level);
-    });
-  }
 };
 }
 
